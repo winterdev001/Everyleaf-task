647a61c43fdf49c372544002cc8d1951a3f3e573
winterdev001 <snowdevin.sd@gmail.com>
2019-10-22 06:18:21 +0000

    added pagination

--- Gemfile
+++ Gemfile
@@ -36,3 +36,4 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'ransack'
+gem 'kaminari'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,11 @@ class TasksController < ApplicationController
     # @tasks = Task.search(params[:search]).order("created_at DESC")
       @tasks = @search.result
     elsif params[:sort_with_created_at]
-      @tasks = Task.order('created_at DESC')
+      @tasks = Task.order('created_at DESC').page(params[:page])
     elsif params[:sort_with_deadline]
-      @tasks = Task.order('deadline ASC')
+      @tasks = Task.order('deadline ASC').page(params[:page])
     else params[:sort_with_priority]
-      @tasks = Task.order('priority ASC')     
+      @tasks = Task.order('priority ASC').page(params[:page])    
     end
     # @search = Task.search(params[:q])
     # @tasks = @search.result
