7773b3a520ea3a7434bb2eddbc6d4596cfccbce6
winterdev001 <snowdevin.sd@gmail.com>
2019-11-16 14:28:35 +0000

    added test for only users to see their task

--- spec/features/user.spec.rb
+++ spec/features/user.spec.rb
@@ -32,6 +32,31 @@ RSpec.feature "user management function", type: :feature do
   visit tasks_path
   expect(page).to have_content "can't access this page unless logged in"
  end
+
+ scenario "display  users task only" do   
+   
+    User.create!(name: 'winter', email: 'winter@gmail.com', password: 'winter')
+    User.create!(name: 'dev', email: 'deva@gmail.com', password: 'dev123')
+    user=User.first
+    @user = User.last
+
+    @task=    Task.create!(task_name: "holla", label_name: "hellohello",start_date: '2019-10-5 1:40:00',status: 'not started', deadline: '2019-10-15 5:40:00' , priority: 'High', user_id: user.id)
+    @task =   Task.create!(task_name: "holla11", label_name: "hellohello11",start_date: '2019-10-5 1:40:00',status: 'not started', deadline: '2019-10-15 5:40:00' , priority: 'High', user_id: @user.id)
+    current_user = User.first
+    
+    visit new_session_path
+     fill_in  'Email' ,  :with => 'deva@gmail.com'  
+     fill_in  'Password' ,  :with => 'dev123'
+     click_on  'Log in'
+
+     visit  tasks_path
+
+    if @task.user_id == current_user.id
+      expect(page).to have_content 'holla11'
+      expect(page).not_to have_content  'holla'
+    end
+  end
+
 end
 
 RSpec.feature "user management function/logged in functions", type: :feature do
@@ -47,12 +72,7 @@ RSpec.feature "user management function/logged in functions", type: :feature do
     expect(page).to have_content "can't access this page unless logged out"
   end
 
-  scenario "display users task only" do
-    User.create!(name: 'winter', email: 'winter@gmail.com', password: 'winter')
-    user=User.first
-    @task=    Task.create!(task_name: "holla", label_name: "hellohello",start_date: '2019-10-5 1:40:00',status: 'not started', deadline: '2019-10-15 5:40:00' , priority: 'High', user_id: user.id)
-    expect(@task.task_name).to match("holla")
-  end
+ 
 
   scenario "Test user details" do
    @user= User.create!(name: 'dev', email: 'winter@gmail.com', password: 'dev123')
